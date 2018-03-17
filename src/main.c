4c57dee76c30ef8ff4b25f4fc5cdc62227890882
Rick Taylor <rick@tropicalstormsoftware.com>
2018-03-17 11:54:28 +0000

    Fixes found building on a new machine

--- src/main.c
+++ src/main.c
@@ -12,7 +12,7 @@ int main(int argc, char* argv[])
 	prolite_env_t dummy;
 	prolite_query_t q;
 
-	prolite_prepare(&dummy,"test(X).",-1,&q,NULL);
+	prolite_prepare(dummy,"test(X).",-1,&q,NULL);
 
 	return 0;
 }
