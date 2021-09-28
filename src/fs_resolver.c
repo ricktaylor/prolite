8fa915a2f0e4fe66d76d9b348fec33ba78241697
Rick Taylor <rick@tropicalstormsoftware.com>
2021-09-28 21:01:25 +0100

    Stop dumpping input

--- src/fs_resolver.c
+++ src/fs_resolver.c
@@ -69,11 +69,11 @@ static int64_t fs_stream_read(struct prolite_stream* s, void* dest, size_t len)
 	if (r == 0 && ferror(f))
 		r = -1;
 
-	if (r > 0)
+	/*if (r > 0)
 	{
 		fwrite(dest,1,r,stdout);
 		fflush(stdout);
-	}
+	}*/
 
 	return r;
 }
