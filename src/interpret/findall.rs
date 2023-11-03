19cbce917f89070172d722428d7164caced76b17
Rick Taylor <rick.taylor@ori.co>
2023-11-03 18:39:00 +0000

    WIP

--- src/interpret/findall.rs
+++ src/interpret/findall.rs
@@ -1,3 +1,5 @@
+use bit_set::BitSet;
+
 use super::*;
 use term::*;
 
@@ -32,15 +34,17 @@ pub(super) fn solve_findall(
     substs: &[Var],
     next: &mut dyn Solver,
 ) -> Response {
-    let mut new_substs: Vec<Var> = Vec::new();
-    let mut solutions: Vec<Rc<Term>> = Vec::new();
+    let mut new_substs = substs.to_vec();
+    let mut solutions = Vec::new();
 
     solve::call(
         ctx,
         &args[1],
         substs,
-        &mut Continuation::new(|_, substs| {
-            solutions.push(renamed_copy(&args[0], substs, &mut new_substs));
+        &mut Continuation::new(|_, inner_substs| {
+            // TODO: We can early out here if args[2] is a list or partial list
+
+            solutions.push(renamed_copy(&args[0], inner_substs, &mut new_substs));
             Response::Fail
         }),
     )
@@ -54,32 +58,98 @@ pub(super) fn solve_findall(
     })
 }
 
-/*pub(super) fn solve_setof(
+fn clear_variable_set(t: &Rc<Term>, substs: &[Var], free_vars: &mut BitSet) {
+    match &t.kind {
+        TermKind::Var(idx) => {
+            if let Some(t) = substs[*idx] {
+                clear_variable_set(t, substs, free_vars);
+            } else {
+                free_vars.remove(*idx);
+            }
+        }
+        TermKind::Compound(c) => {
+            for t in c.args.iter() {
+                clear_variable_set(t, substs, free_vars);
+            }
+        }
+        _ => {}
+    }
+}
+
+fn iterate_goal<'a>(t: &'a Rc<Term>, substs: &[Var<'a>], free_vars: &mut BitSet) -> &'a Rc<Term> {
+    match &t.kind {
+        TermKind::Var(idx) => {
+            if let Some(t) = substs[*idx] {
+                iterate_goal(t, substs, free_vars)
+            } else {
+                t
+            }
+        }
+        TermKind::Compound(c) if c.functor == "^" && c.args.len() == 2 => {
+            clear_variable_set(&c.args[0], substs, free_vars);
+            iterate_goal(&c.args[1], substs, free_vars)
+        }
+        _ => t,
+    }
+}
+
+fn deref_var<'a>(mut t: &'a Rc<Term>, substs: &[Var<'a>]) -> &'a Rc<Term> {
+    match &t.kind {
+        TermKind::Var(idx) => {
+            if let Some(t) = substs[*idx] {
+                deref_var(t, substs)
+            } else {
+                t
+            }
+        }
+        _ => t,
+    }
+}
+
+pub(super) fn solve_setof(
     ctx: &mut Context,
-    args: &[Term],
+    args: &[Rc<Term>],
     substs: &[Var],
     next: &mut dyn Solver,
 ) -> Response {
+    // Find the free variables of the iterated goal of arg[1] wrt arg[0]
+    let mut free_vars = BitSet::with_capacity(substs.len());
+    for (idx, v) in substs.iter().enumerate() {
+        if v.is_none() {
+            free_vars.insert(idx);
+        }
+    }
+    clear_variable_set(&args[0], substs, &mut free_vars);
 
-    let witness = witness_set(&args[0],&args[1],substs);
-    let goal = iterated_goal(&args[1]);
-
-    let mut new_substs: Vec<Var> = Vec::new();
-    let mut solutions: Vec<(Term,Term)> = Vec::new();
-
+    let mut new_substs = substs.to_vec();
+    let mut solutions = Vec::new();
     solve::call(
         ctx,
-        &goal,
+        iterate_goal(&args[1], substs, &mut free_vars),
         substs,
-        &mut Continuation::new(|_, substs| {
-            solutions.push((renamed_copy(&witness, substs, &mut new_substs),renamed_copy(&args[0], substs, &mut new_substs)));
+        &mut Continuation::new(|_, inner_substs| {
+            // Accumulate the current values of the free variables
+            let free_values: Vec<Option<Rc<Term>>> = free_vars
+                .iter()
+                .map(|idx| {
+                    inner_substs[idx].map(|t| {
+                        // TODO -  We can inline deref into a loop
+                        deref_var(t, inner_substs).clone()
+                    })
+                })
+                .collect();
+
+            let template = renamed_copy(&args[0], inner_substs, &mut new_substs);
+
+            //MUCH MORE HERE!
+
+            solutions.push((free_values, template));
             Response::Fail
         }),
-    ).map_failed(|| {
-        while !solutions.is_empty() {
-
-        }
+    )
+    .map_failed(|| {
+        while !solutions.is_empty() {}
 
         Response::Fail
     })
-}*/
+}
---
+++ src/interpret/write.rs
@@ -0,0 +1,29 @@
+use super::*;
+use term::*;
+
+pub(super) fn solve_write(
+    ctx: &mut Context,
+    args: &[Rc<Term>],
+    substs: &[Var],
+    next: &mut dyn Solver,
+) -> Response {
+    let mut term = if args.len() == 2 { &args[1] } else { &args[0] };
+    while let term::TermKind::Var(idx) = &term.kind {
+        match &substs[*idx] {
+            None => break,
+            Some(t) => term = t,
+        }
+    }
+    print!("{:?}", *term);
+    next.solve(ctx, substs)
+}
+
+pub(super) fn solve_nl(
+    ctx: &mut Context,
+    _: &[Rc<Term>],
+    substs: &[Var],
+    next: &mut dyn Solver,
+) -> Response {
+    println!();
+    next.solve(ctx, substs)
+}
