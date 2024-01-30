043bc0d8f0e6fdaddec2e3797b76b2469de28049
Rick Taylor <rick.taylor@ori.co>
2024-01-30 17:04:05 +0000

    Fixed compare

--- src/interpret/arithmetic.rs
+++ src/interpret/arithmetic.rs
@@ -8,71 +8,143 @@ use term::TermKind;
 fn throw_evaluable(term: &Rc<read_term::Term>) -> Response {
     throw::error(
         read_term::Term::new_compound(
-            "evaluable".to_string(),
+            "type_error".to_string(),
             None,
-            vec![term.as_pi().unwrap_or(term.clone())],
+            vec![
+                read_term::Term::new_atom("evaluable".to_string(), None),
+                term.as_pi().unwrap_or(term.clone()),
+            ],
         ),
         term.location.clone(),
     )
 }
 
-fn compare_with_overflow(location: &Option<stream::Span>, i: &i64) -> Result<f64, Response> {
-    let f = *i as f64;
-    if *i != f as i64 {
-        Err(throw::error(
-            read_term::Term::new_compound(
-                "evaluation_error".to_string(),
-                None,
-                vec![read_term::Term::new_atom(
-                    "float_overflow".to_string(),
-                    None,
-                )],
-            ),
-            location.clone(),
-        ))
-    } else {
-        Ok(f)
+enum ValueKind {
+    Integer(i64),
+    Float(f64),
+}
+
+struct Value {
+    kind: ValueKind,
+    location: Option<stream::Span>,
+}
+
+impl Value {
+    fn new_integer(i: i64, location: &Option<stream::Span>) -> Self {
+        Self {
+            kind: ValueKind::Integer(i),
+            location: location.clone(),
+        }
+    }
+
+    fn new_float(f: f64, location: &Option<stream::Span>) -> Self {
+        Self {
+            kind: ValueKind::Float(f),
+            location: location.clone(),
+        }
+    }
+
+    fn to_float(&self) -> Result<f64, Response> {
+        match &self.kind {
+            ValueKind::Integer(i) => {
+                let f = *i as f64;
+                if *i != f as i64 {
+                    Err(throw::error(
+                        read_term::Term::new_compound(
+                            "evaluation_error".to_string(),
+                            None,
+                            vec![read_term::Term::new_atom(
+                                "float_overflow".to_string(),
+                                None,
+                            )],
+                        ),
+                        self.location.clone(),
+                    ))
+                } else {
+                    Ok(f)
+                }
+            }
+            ValueKind::Float(f) => Ok(*f),
+        }
+    }
+}
+
+fn eval(frame: &Frame, expr: usize) -> Result<Value, Response> {
+    let (expr, _) = frame.get_term(expr);
+    match (&expr.kind, &expr.source.kind) {
+        (TermKind::Atomic, read_term::TermKind::Integer(i)) => {
+            Ok(Value::new_integer(*i, &expr.source.location))
+        }
+        (TermKind::Atomic, read_term::TermKind::Float(f)) => {
+            Ok(Value::new_float(*f, &expr.source.location))
+        }
+        (TermKind::Atomic, read_term::TermKind::Atom(s)) => {
+            match get_builtin(&format!("{}/0", s)) {
+                Some(f) => (f)(&[], &expr.source.location),
+                None => Err(throw_evaluable(&expr.source)),
+            }
+        }
+        (TermKind::Var(_), read_term::TermKind::Var(_)) => {
+            Err(throw::instantiation_error(&expr.source))
+        }
+        (TermKind::Compound(args), read_term::TermKind::Compound(c)) => {
+            match get_builtin(&format!("{}/{}", c.functor, args.len())) {
+                Some(f) => {
+                    let mut values = Vec::new();
+                    for a in args.iter() {
+                        values.push(eval(frame, *a)?);
+                    }
+                    (f)(&values, &expr.source.location)
+                }
+                None => Err(throw_evaluable(&expr.source)),
+            }
+        }
+        _ => unreachable!(),
+    }
+}
+
+pub fn solve_is(mut frame: Frame, args: &[usize], next: &mut dyn Solver) -> Response {
+    match eval(&frame, args[1]) {
+        Err(r) => r,
+        Ok(v) => match &v.kind {
+            ValueKind::Integer(i) => frame.sub_frame(|mut frame| {
+                let expr = frame.new_term(&read_term::Term::new_integer(*i, v.location));
+                if frame.unify(args[0], expr) {
+                    next.solve(frame)
+                } else {
+                    Response::Fail
+                }
+            }),
+            ValueKind::Float(f) => frame.sub_frame(|mut frame| {
+                let expr = frame.new_term(&read_term::Term::new_float(*f, v.location));
+                if frame.unify(args[0], expr) {
+                    next.solve(frame)
+                } else {
+                    Response::Fail
+                }
+            }),
+        },
+    }
+}
+
+fn promote2<R, FI: FnOnce(i64, i64) -> R, FD: FnOnce(f64, f64) -> R>(
+    v1: &Value,
+    v2: &Value,
+    fi: FI,
+    fd: FD,
+) -> Result<R, Response> {
+    match (&v1.kind, &v2.kind) {
+        (ValueKind::Integer(i1), ValueKind::Integer(i2)) => Ok((fi)(*i1, *i2)),
+        (ValueKind::Integer(_), ValueKind::Float(f2)) => Ok((fd)(v1.to_float()?, *f2)),
+        (ValueKind::Float(f1), ValueKind::Integer(_)) => Ok((fd)(*f1, v2.to_float()?)),
+        (ValueKind::Float(f1), ValueKind::Float(f2)) => Ok((fd)(*f1, *f2)),
     }
 }
 
 fn compare(frame: &Frame, t1: usize, t2: usize) -> Result<Option<core::cmp::Ordering>, Response> {
-    let (t1, _) = frame.get_term(t1);
-    match (&t1.kind, &t1.source.kind) {
-        (TermKind::Atomic, read_term::TermKind::Integer(i1)) => {
-            let (t2, _) = frame.get_term(t2);
-            match (&t2.kind, &t2.source.kind) {
-                (TermKind::Atomic, read_term::TermKind::Integer(i2)) => {
-                    Ok(core::cmp::PartialOrd::partial_cmp(i1, i2))
-                }
-                (TermKind::Atomic, read_term::TermKind::Float(f2)) => {
-                    Ok(core::cmp::PartialOrd::partial_cmp(
-                        &compare_with_overflow(&t1.source.location, i1)?,
-                        f2,
-                    ))
-                }
-                (TermKind::Var(_), _) => Err(throw::instantiation_error(&t2.source)),
-                _ => Err(throw_evaluable(&t2.source)),
-            }
-        }
-        (TermKind::Atomic, read_term::TermKind::Float(f1)) => {
-            let (t2, _) = frame.get_term(t2);
-            match (&t2.kind, &t2.source.kind) {
-                (TermKind::Atomic, read_term::TermKind::Integer(i2)) => {
-                    Ok(core::cmp::PartialOrd::partial_cmp(
-                        f1,
-                        &compare_with_overflow(&t2.source.location, i2)?,
-                    ))
-                }
-                (TermKind::Atomic, read_term::TermKind::Float(f2)) => {
-                    Ok(core::cmp::PartialOrd::partial_cmp(f1, f2))
-                }
-                (TermKind::Var(_), _) => Err(throw::instantiation_error(&t2.source)),
-                _ => Err(throw_evaluable(&t2.source)),
-            }
-        }
-        (TermKind::Var(_), _) => Err(throw::instantiation_error(&t1.source)),
-        _ => Err(throw_evaluable(&t1.source)),
-    }
+    let e1 = eval(frame, t1)?;
+    let e2 = eval(frame, t2)?;
+    promote2(&e1, &e2, |a, b| a.partial_cmp(&b), |a, b| a.partial_cmp(&b))
 }
 
 pub fn solve_eq(frame: Frame, args: &[usize], next: &mut dyn Solver) -> Response {
@@ -127,111 +199,31 @@ pub fn solve_geq(frame: Frame, args: &[usize], next: &mut dyn Solver) -> Respons
     }
 }
 
-enum Value {
-    Integer(i64),
-    Float(f64),
-}
-
-impl Value {
-    fn to_float(&self, location: &Option<stream::Span>) -> Result<f64, Response> {
-        match self {
-            Value::Integer(i) => {
-                let f = *i as f64;
-                if *i != f as i64 {
-                    Err(throw::error(
-                        read_term::Term::new_compound(
-                            "evaluation_error".to_string(),
-                            None,
-                            vec![read_term::Term::new_atom(
-                                "float_overflow".to_string(),
-                                None,
-                            )],
-                        ),
-                        location.clone(),
-                    ))
-                } else {
-                    Ok(f)
-                }
+fn eval_add(args: &[Value], location: &Option<stream::Span>) -> Result<Value, Response> {
+    promote2(
+        &args[0],
+        &args[1],
+        |a, b| match a.checked_add(b) {
+            None => {
+                todo!()
             }
-            Value::Float(f) => Ok(*f),
-        }
-    }
-}
-
-fn eval(frame: &Frame, expr: usize) -> Result<Value, Response> {
-    let (expr, _) = frame.get_term(expr);
-    match (&expr.kind, &expr.source.kind) {
-        (TermKind::Atomic, read_term::TermKind::Integer(i)) => Ok(Value::Integer(*i)),
-        (TermKind::Atomic, read_term::TermKind::Float(f)) => Ok(Value::Float(*f)),
-        (TermKind::Atomic, read_term::TermKind::Atom(s)) => {
-            match get_builtin(&format!("{}/0", s)) {
-                Some(f) => (f)(&[], &expr.source.location),
-                None => Err(throw_evaluable(&expr.source)),
-            }
-        }
-        (TermKind::Var(_), read_term::TermKind::Var(_)) => {
-            Err(throw::instantiation_error(&expr.source))
-        }
-        (TermKind::Compound(args), read_term::TermKind::Compound(c)) => {
-            match get_builtin(&format!("{}/{}", c.functor, args.len())) {
-                Some(f) => {
-                    let mut values = Vec::new();
-                    for a in args.iter() {
-                        values.push(eval(frame, *a)?);
-                    }
-                    (f)(&values, &expr.source.location)
-                }
-                None => Err(throw_evaluable(&expr.source)),
-            }
-        }
-        _ => unreachable!(),
-    }
-}
-
-pub fn solve_is(mut frame: Frame, args: &[usize], next: &mut dyn Solver) -> Response {
-    match eval(&frame, args[1]) {
-        Err(r) => r,
-        Ok(Value::Integer(i)) => frame.sub_frame(|mut frame| {
-            let expr = frame.new_term(&read_term::Term::new_integer(i, None));
-            if frame.unify(args[0], expr) {
-                next.solve(frame)
-            } else {
-                Response::Fail
-            }
-        }),
-        Ok(Value::Float(d)) => frame.sub_frame(|mut frame| {
-            let expr = frame.new_term(&read_term::Term::new_float(d, None));
-            if frame.unify(args[0], expr) {
-                next.solve(frame)
-            } else {
-                Response::Fail
-            }
-        }),
-    }
-}
-
-fn promote2<R, FI: FnOnce(i64, i64) -> R, FD: FnOnce(f64, f64) -> R>(
-    v1: &Value,
-    v2: &Value,
-    fi: FI,
-    fd: FD,
-    location: &Option<stream::Span>,
-) -> Result<R, Response> {
-    match (v1, v2) {
-        (Value::Integer(i1), Value::Integer(i2)) => Ok((fi)(*i1, *i2)),
-        (Value::Integer(_), Value::Float(f2)) => Ok((fd)(v1.to_float(location)?, *f2)),
-        (Value::Float(f1), Value::Integer(_)) => Ok((fd)(*f1, v2.to_float(location)?)),
-        (Value::Float(f1), Value::Float(f2)) => Ok((fd)(*f1, *f2)),
-    }
+            Some(c) => Value::new_integer(c, location),
+        },
+        |a, b| Value::new_float(a + b, location),
+    )
 }
 
 fn eval_sub(args: &[Value], location: &Option<stream::Span>) -> Result<Value, Response> {
     promote2(
         &args[0],
         &args[1],
-        |a, b| Value::Integer(a - b),
-        |a, b| Value::Float(a - b),
-        location,
+        |a, b| match a.checked_sub(b) {
+            None => {
+                todo!()
+            }
+            Some(c) => Value::new_integer(c, location),
+        },
+        |a, b| Value::new_float(a - b, location),
     )
 }
 
@@ -239,9 +231,13 @@ fn eval_mul(args: &[Value], location: &Option<stream::Span>) -> Result<Value, Re
     promote2(
         &args[0],
         &args[1],
-        |a, b| Value::Integer(a * b),
-        |a, b| Value::Float(a * b),
-        location,
+        |a, b| match a.checked_mul(b) {
+            None => {
+                todo!()
+            }
+            Some(c) => Value::new_integer(c, location),
+        },
+        |a, b| Value::new_float(a * b, location),
     )
 }
 
@@ -258,7 +254,7 @@ fn not_impl(_args: &[Value], location: &Option<stream::Span>) -> Result<Value, R
 type EvalFn = fn(args: &[Value], location: &Option<stream::Span>) -> Result<Value, Response>;
 
 const BUILTINS: phf::Map<&'static str, EvalFn> = phf_map! {
-    "+/2" => not_impl,
+    "+/2" => eval_add,
     "-/2" => eval_sub,
     "*/2" => eval_mul,
     "///2" => not_impl,
